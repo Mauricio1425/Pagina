70b85f0708fb4d67a977760dd5a19fa6ae8cd615
Mauricio1425 <mauricio.tavitas25@gmail.com>
2023-07-14 16:25:30 -0600

    php_pb1_newCode

--- enviar.php
+++ enviar.php
@@ -1,20 +1,40 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $to = 'mauricio.tavitas25@gmail.com'; 
-    $subject = $_POST['subjet'];
+    $to = 'mauricio.tavitas25@gmail.com';
+    $from = 'mauricio.tavitas25@gmail.com';
     $name = $_POST['name'];
-    $from = $_POST['mail'];
+    $email = $_POST['mail'];
+    $subject = $_POST['subjet'];
     $message = $_POST['message'];
 
-    $headers = "From: $name <$from>" . "\r\n";
-    $headers .= "Reply-To: $from" . "\r\n";
-    $headers .= "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
+    // Validar campos requeridos
+    if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+        echo 'Por favor completa todos los campos.';
+        exit;
+    }
 
-    if (mail($to, $subject, $message, $headers)) {
-        echo "El correo se envió correctamente.";
+    // Validar dirección de correo electrónico
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo 'Por favor introduce una dirección de correo electrónico válida.';
+        exit;
+    }
+
+    // Construir el mensaje
+    $email_message = "Nombre: $name\n";
+    $email_message .= "Correo Electrónico: $email\n";
+    $email_message .= "Asunto: $subject\n";
+    $email_message .= "Mensaje:\n$message\n";
+
+    // Configurar cabeceras del correo
+    $headers = "From: $from\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Enviar el correo
+    if (mail($to, $subject, $email_message, $headers)) {
+        echo 'Tu mensaje ha sido enviado exitosamente.';
     } else {
-        echo "Hubo un error al enviar el correo.";
+        echo 'Ha ocurrido un error al enviar el mensaje. Por favor, intenta nuevamente.';
     }
 }
 ?>
+
